0aa4bbf48c3b1c44505aa694fcda0941ab271e83
thanhlam0822 <benzamano123@gmail.com>
2021-05-17 10:21:41 +0700

    first commit

---
+++ baitapchuong5/TruongPhong.java
@@ -0,0 +1,48 @@
+package baitapchuong5;
+
+import java.util.Scanner;
+
+public class TruongPhong extends NhanVien {
+  public   double phuCap;
+  public  int soNamDuongChuc;
+
+    public TruongPhong() {
+
+    }
+    public TruongPhong(String tenNhanVien,double heSoLuong, double luongCoBan, double LUONG_MAX) {
+        super(tenNhanVien,heSoLuong,luongCoBan,LUONG_MAX);
+
+    }
+
+    public double getPhuCap() {
+        return phuCap;
+    }
+
+    public int getSoNamDuongChuc() {
+        return soNamDuongChuc;
+    }
+
+    public void setPhuCap(double phuCap) {
+        this.phuCap = phuCap;
+    }
+
+    public void setSoNamDuongChuc(int soNamDuongChuc) {
+        this.soNamDuongChuc = soNamDuongChuc;
+    }
+    @Override
+    public void Input() {
+        Scanner scanner = new Scanner(System.in);
+        super.Input();
+        System.out.println(" Nhap phu cap");
+        phuCap = scanner.nextDouble();
+        System.out.println("Nhap so nam duong chuc");
+        soNamDuongChuc = scanner.nextInt();
+    }
+    @Override
+    public void Display() {
+        super.Display();
+        System.out.println(" phu cap" +phuCap);
+        System.out.println("so nam duong chuc" + soNamDuongChuc);
+    }
+
+}
